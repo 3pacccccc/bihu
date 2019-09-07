9592d217146f1ff25c03ed996a7b39f70b436f42
马锐敏 <351489917@qq.com>
2019-09-07 13:53:01 +0800

    添加https证书验证函数

--- apps/users/views.py
+++ apps/users/views.py
@@ -1,8 +1,13 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+
 # Create your views here.
 
 
 def test(request):
     return HttpResponse("hello 5", content_type='application/json')
+
+
+def verify(request):
+    return HttpResponse('pycB7iWgN8uTQBUx8PiEjsRfPlqOXglff63pCTCLtsM.vQCFhMEdspQRFGVJje2AbyO-qe2ynoA7WcDqKELIIoA', content_type='application/json')
