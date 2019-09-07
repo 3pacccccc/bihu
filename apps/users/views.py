e8f0b9904d1bcfad0dcdec9e5233bb6d3d5f2b30
马锐敏 <351489917@qq.com>
2019-09-07 10:51:21 +0800

    test

--- apps/users/views.py
+++ apps/users/views.py
@@ -5,4 +5,4 @@ from django.shortcuts import render
 
 
 def test(request):
-    return HttpResponse("hello44", content_type='application/json')
+    return HttpResponse("hello55", content_type='application/json')
