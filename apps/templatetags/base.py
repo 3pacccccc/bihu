c8f785368e8a56dd36e1046bb4d4435415224c2d
马锐敏 <351489917@qq.com>
2019-10-14 17:32:43 +0800

    markdown代码块

--- apps/templatetags/base.py
+++ apps/templatetags/base.py
@@ -1,1 +1,22 @@
 # -*- coding: utf-8 -*-
+
+import markdown
+from django import template
+from django.template.defaultfilters import stringfilter
+from django.utils.encoding import force_text
+from django.utils.safestring import mark_safe
+
+
+register = template.Library()
+
+
+@register.filter(is_safe=True)
+def custom_markdown(value):
+    return mark_safe(markdown.markdown(value,
+                                       extensions=['markdown.extensions.extra',
+                                                   'markdown.extensions.toc',
+                                                   'markdown.extensions.sane_lists',
+                                                   'markdown.extensions.nl2br',
+                                                   'markdown.extensions.codehilite', ],
+                                       safe_mode=True,
+                                       enable_attributes=False))
